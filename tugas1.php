bee7cf39eb1f180c01c8a8ee92199526ff603e17
Trisantiana <trisantiana9@gmail.com>
2017-10-06 17:14:30 +0700

    tugas1

---
+++ tugas1.php
@@ -0,0 +1,110 @@
+<?php
+echo "<font color= black face='Georgia'>";
+
+
+
+
+echo "<br>";
+echo "<br>";
+
+echo "<B>";
+echo "<center>";
+echo "BIODATA";
+echo "<br>";
+echo "<br>";
+
+echo "<table width='560' border='0'>";
+  echo "<tr>";
+    echo "<td width='51'>";
+        echo "&nbsp;";
+        echo "</td>";
+    echo "<td width='227'>";
+        echo "Nama Lengkap";
+        echo "</td>";
+    echo "<td width='8'>";
+        echo ":";
+        echo "</td>";
+    echo "<td width='254'>";
+        echo "Tri Santiana Dewi";
+        echo "</td>";
+  echo "</tr>";
+
+  echo "<tr>";
+    echo "<td>";
+        echo "&nbsp;";
+        echo "</td>";
+    echo "<td>";
+        echo "Nama Panggilan";
+        echo "</td>";
+    echo "<td>";
+        echo ":";
+        echo "</td>";
+    echo "<td>";
+        echo "Tri";
+        echo "</td>";
+  echo "</tr>";
+  echo "<tr>";
+    echo "<td>";
+        echo "&nbsp;";
+        echo "</td>";
+       
+    echo "<td>";
+        echo "Tempat, Tanggal Lahir";
+        echo "</td>";
+    echo "<td>";
+        echo ":";
+        echo "</td>";
+    echo "<td>";
+        echo "Ponorogo, 03 September 2001";
+        echo "</td>";
+  echo "</tr>";
+
+  echo "<tr>";
+    echo "<td>";
+        echo "&nbsp;";
+        echo "</td>";
+    echo "<td>";
+        echo "Asal Sekolah";
+        echo "</td>";
+    echo "<td>";
+        echo ":";
+        echo "</td>";
+    echo "<td>";
+        echo "SMKN 1 Ponorogo";
+        echo "</td>";
+  echo "</tr>";
+  echo "<tr>";
+    echo "<td>";
+        echo "&nbsp;";
+        echo "</td>";
+       
+    echo "<td>";
+        echo "Alamat";
+        echo "</td>";
+    echo "<td>";
+        echo ":";
+        echo "</td>";
+    echo "<td>";
+        echo "Ds.Simo, Kec.Slahung, Ponorogo";
+        echo "</td>";
+  echo "</tr>";
+ 
+  
+  echo "<tr>";
+    echo "<td>";
+        echo "&nbsp;";
+        echo "</td>";
+    echo "<td>";
+        echo "Agama";
+        echo "</td>";
+    echo "<td>";
+        echo ":";
+        echo "</td>";
+    echo "<td>";
+        echo "Islam";
+        echo "</td>";
+  echo "</tr>";
+
+  
+echo "</table>";
+?>
